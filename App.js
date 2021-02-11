bc856d6fdc9fd54a87e0006afa5574bea160db3a
Miguel Morales <miguel.morales@24imedia.com>
2021-02-11 10:03:37 +0100

    Add native features

--- App.js
+++ App.js
@@ -14,25 +14,10 @@ import { StatusBar, View } from 'react-native';
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
+import { setLocalNotification } from './utils/helpers'
 
 const Stack = createStackNavigator();
-
-const StackConfigs = {
-  home: {
-    screen: Tabs,
-    navigationOptions: {
-      header: null,
-    },
-  },
-  EntryDetail: {
-    screenOptions: {
-      headerTintColor: white,
-      headerStyle: {
-        backgroundColor: purple,
-      },
-    },
-  },
-};
 
 const RouteConfigs = {
   History:{
@@ -44,6 +29,11 @@ const RouteConfigs = {
     component: AddEntry,
     name: "Add Entry",
     options: {tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />, title: 'Add Entry'}
+  },
+  Live:{
+    component: Live,
+    name: "Live",
+    options: {tabBarIcon: ({tintColor}) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />, title: 'Live'}
   }
 }
 
@@ -83,6 +73,7 @@ const Tabs = () => (
     <Tab.Navigator {...TabNavigatorConfig}>
         <Tab.Screen {...RouteConfigs['History']} />
         <Tab.Screen {...RouteConfigs['AddEntry']} />
+        <Tab.Screen {...RouteConfigs['Live']} />
     </Tab.Navigator>
 )
 const MainNav = () => (
@@ -103,6 +94,10 @@ const MainNav = () => (
 );
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    setLocalNotification()
+  }
 
   render() {
     return (
--- utils/helpers.js
+++ utils/helpers.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
 import { red, orange, blue, lightPurp, pink, white } from './colors'
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
+import AsyncStorage from '@react-native-community/async-storage'
+
+const NOTIFICATION_KEY = 'UdaciFitness:notifications'
 
 export function isBetween (num, x, y) {
   if (num >= x && num <= y) {
@@ -160,4 +165,55 @@ export function getDailyReminderValue () {
   return {
     today: "👋 Don't forget to log your data today!"
   }
+}
+
+export function clearLocalNotification () {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+}
+
+function createNotification () {
+  return {
+    title: 'Log your stats!',
+    body: "👋 don't forget to log your stats for today!",
+    ios: {
+      sound: true,
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true,
+    }
+  }
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+      }
+    })
 }
